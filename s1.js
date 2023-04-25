ef0baf97bba6c6b87b9f87b67fbb3f02f82a656f
Smoz1e <113229013+Smoz1e@users.noreply.github.com>
2023-04-25 19:42:06 +0300

    Add files via upload

---
+++ s1.js
@@ -0,0 +1,20 @@
+// Adjust video height for mobile devices
+window.onload = function() {
+    
+   
+    var video = document.querySelector('video');
+        
+       
+    if (window.innerWidth < 768) {
+            video.
+            video
+    
+           
+    style.height = 'auto';
+        }
+    }
+    
+
+    
+       
+    // Change nav background
